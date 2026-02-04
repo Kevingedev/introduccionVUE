b724a7df574a36a9aeea90cf17c4929b548c4277
jorge <jorgecerecedatrabajo@gmail.com>
2026-02-04 08:31:01 +0100

    cambios esteticos

--- src/components/MiComponente.vue
+++ src/components/MiComponente.vue
@@ -3,11 +3,8 @@
     <p class="texto">Ejemplo de uso de directivas</p>
 
 
-
-
     <!--v-on cambia el valor de visible-->
-    <button @click="ocultarBoton">{{ textoboton }}</button>
-
+    <button @click="ocultarBoton" class="boton">{{ textoboton }}</button>
 
 
     <div class="cuadro">
@@ -25,11 +22,12 @@
 
     </div>
 
-    <button @click="anadirLista">añadir usuario</button>
+    <!--v-on ejecuta accion al clickar-->
+    <button @click="anadirLista" class="boton">añadir usuario</button>
 
     <div class="cuadro">
 
-      <!--formulario: v-model-->
+      <!--v-model gestiona formularios-->
       <input v-model="nombreusuario" type="nombreusuario" placeholder="Escribe el nombre" />
 
       <p>nombre ingresado: {{ nombreusuario }}</p>
@@ -123,15 +121,35 @@ function ocultarBoton() {
 <style scoped>
 .texto {
   color: blue;
-  font-size: 18px;
+  font-size: 2rem;
   margin: 20px;
 }
 
 .cuadro {
-  border-style: solid;
   border-radius: 10px;
   border-width: 1px;
   padding: 20px;
+  margin: 20px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 }
+
+.cuadro p{
+  font-size: large;
+}
+
+.cuadro li{
+  font-size: large;
+}
+
+.boton{
+  background-color: #008CBA; /* Color de fondo */
+  color: white; /* Color del texto */
+  padding: 15px 30px; /* Tamaño interno */
+  border: none; /* Quitar borde predeterminado */
+  border-radius: 5px; /* Bordes redondeados */
+  cursor: pointer; /* Cambia el cursor a mano */
+  font-size: 16px;
+}
+
 </style>
 
