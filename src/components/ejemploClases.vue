32453075c400dca11d71365bd0d6d072afd9e738
jorge <jorgecerecedatrabajo@gmail.com>
2026-02-04 08:40:26 +0100

    cambios

---
+++ src/components/ejemploClases.vue
@@ -0,0 +1,44 @@
+<template>
+    <h2 class="texto">Ejemplo de clases CSS dinamicas</h2>
+
+<!--texto con clases dinamicas-->
+ <div class="cuadro">
+ <p :class="{ activo: estaActivo, error: hayError }">
+    Este es un ejemplo de clase dinámica.
+ </p>
+<p>Esta activo: {{ estaActivo }}</p>
+
+
+<!--Alterna las clases al clickar-->
+<button @click="cambiar">
+    Toggle
+</button>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const estaActivo = ref(false);
+const hayError = ref(true);
+
+function cambiar(){
+    estaActivo.value=!estaActivo.value;
+    hayError.value=!hayError.value;
+}
+
+</script>
+
+<!-- Estilos scoped, solo validos en el componente -->
+<style scoped>
+    .activo{
+         color: rgb(75, 255, 99);
+        font-weight: bold;
+        padding: 10px;
+    }
+      .error{
+         color: rgb(255, 156, 75);
+        font-weight: bold;
+        padding: 10px;
+    }
+</style>
